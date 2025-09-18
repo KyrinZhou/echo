bfe980640eaf9b200de5ef51f9d2774c152c1c77
zhouqilin <zhouqilin@tabcut.com>
2025-09-18 14:55:00 +0800

    03: clerk authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,9 @@
 
 import { useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
   const users = useQuery(api.user.getMany);
@@ -9,7 +12,15 @@ export default function Page() {
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">app/Web</h1>
-        <pre>{JSON.stringify(users)}</pre>
+        <Authenticated>
+          <UserButton />
+          <pre>{JSON.stringify(users)}</pre>
+        </Authenticated>
+        <Unauthenticated>
+          <SignInButton>
+            <Button>Sign in</Button>
+          </SignInButton>
+        </Unauthenticated>
       </div>
     </div>
   );
