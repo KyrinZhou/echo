5a69bf6f322e25eda6e3c1be1a0a9ba4c0fbf0f3
zhouqilin <zhouqilin@tabcut.com>
2025-09-28 11:06:36 +0800

    05 ai-assistant voice

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,30 @@
 "use client";
 
-import { useQuery } from "convex/react";
-import { api } from "@workspace/backend/_generated/api";
+import { useVapi } from "@/modules/widget/hooks/use-vapi";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
-  const users = useQuery(api.user.getMany);
+  const {
+    isConnected,
+    isSpeaking,
+    transcript,
+    isConecting,
+    startCall,
+    endCall,
+  } = useVapi();
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello widget</h1>
-        {JSON.stringify(users)}
+        <Button onClick={startCall}>Start Call</Button>
+        <Button variant="destructive" onClick={endCall}>
+          End Call
+        </Button>
+        <p>isConnected: {isConnected ? "true" : "false"}</p>
+        <p>isSpeaking: {isSpeaking ? "true" : "false"}</p>
+        <p>isConecting: {isConecting ? "true" : "false"}</p>
+        <pre>{JSON.stringify(transcript)}</pre>
       </div>
     </div>
   );
